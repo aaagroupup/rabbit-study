3d184d09f3a35ba1a5ae4ad5730ee015b932df78
ssb <2559525655@qq.com>
2023-03-08 22:14:47 +0800

    修改放行路径

--- service/service_user/src/main/java/com/rabbit/studyweb/config/InterceptorConfig.java
+++ service/service_user/src/main/java/com/rabbit/studyweb/config/InterceptorConfig.java
@@ -8,12 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
+    //放行路径
+    private static final String[] EXCLUDE_PATH={
+            "/teacher/inner/**",
+            "/swagger-ui/**",
+            "/common/**",
+            "/login/**",
+            "/aliPay/**",
+            "/user/updatePassword",
+            "/subject/**",
+            "/vod/**",
+            "/home/**"
+    };
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor())
                 .addPathPatterns("/**")  // 拦截所有请求，通过判断token是否合法来决定是否需要登录
-                .excludePathPatterns("/teacher/inner/**","/swagger-ui/**","/common/**","/login/**","/aliPay/**","/user/updatePassword","/subject/**","/vod/**")
+                .excludePathPatterns(EXCLUDE_PATH)
                 .excludePathPatterns("/**/*.html","/**/*.css","/**/*.js");//放行静态资源
     }
 
